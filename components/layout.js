66b359d52847d50eb066b0a6acbd5b16e1f1c97b
Mitema Emmanuel <mitema.emmanuel@gmail.com>
2022-12-29 15:43:25 +0100

    dynamic routes added

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import styles from './layout.module.css';
 import Head from 'next/head';
 import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link'
 
 
 const name  = "Mitema Emmanuel";
